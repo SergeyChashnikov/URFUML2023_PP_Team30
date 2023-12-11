7d7f920db518dcaa157fc81f023ceafc811b424f
Baddogel <baddogel@gmail.com>
2023-12-11 18:10:39 +0300

    Inserted model rut5-base-multitask

--- model.py
+++ model.py
@@ -1,15 +1,18 @@
 import streamlit as st
-import time
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # Тут будет встраиваться модель
 @st.cache_resource
 def loadmodel():
-  time.sleep(3)
-  return ''
+  tokenizer = T5Tokenizer.from_pretrained("cointegrated/rut5-base-multitask")
+  model_rut5 = T5ForConditionalGeneration.from_pretrained("cointegrated/rut5-base-multitask")
+  return tokenizer, model_rut5
 
 
 # Тут будет распологаться процессинг
-def process(criteria: str):
-  with open("result.txt") as result:
-    time.sleep(3)
-    return result.read()
+def process(criteria: str, tokenizer, model_rut5):
+  inputs = tokenizer(criteria, return_tensors='pt')
+  with torch.no_grad():
+    hypotheses = model_rut5.generate(**inputs, num_beams=5, max_length=300, length_penalty=2.5, no_repeat_ngram_size=3)
+  return tokenizer.decode(hypotheses[0], skip_special_tokens=True)
