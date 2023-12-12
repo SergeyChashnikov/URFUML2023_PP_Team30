826c36ded29b79a5679bd73e4d78c958bda30129
Viktor Ilin <viktor125142@gmail.com>
2023-12-12 16:59:13 +0300

    fix: Изменил функцию запуска, вывод текста

--- src/main.py
+++ src/main.py
@@ -1,48 +1,64 @@
 from app import session, components, constants
 import model
+import streamlit as st
 
 
 def main():
-  # Инициализация состояния приложения
-  session.init()
+    # Инициализация состояния приложения
+    session.init()
 
-  # Рисуем шапку + описание
-  components.header(
-    constants.LANG_PACK.get("title"),
-    constants.LANG_PACK.get("subtitle"),
-    constants.LANG_PACK.get("description"),
-  )
+    # Рисуем шапку + описание
+    components.header(
+        constants.LANG_PACK.get("title"),
+        constants.LANG_PACK.get("subtitle"),
+        constants.LANG_PACK.get("description"),
+    )
 
-  # Подгружаем и кешируем модельку
-  with components.spinner(constants.LANG_PACK.get("loading_model_text")):
-    tokenizer, model_rut5 = model.loadmodel()
+    # Подгружаем и кешируем модельку
+    with components.spinner(constants.LANG_PACK.get("loading_model_text")):
+        tokenizer, model_rut5 = model.loadmodel()
 
-  # Получаем состояние приложения
-  state = session.get_state()
-  # Рисуем основные компоненты приложения, используя состояние
-  text_area_data = components.text_area(
-    constants.LANG_PACK.get("text_area_label"),
-    state.text_area_disabled,
-    constants.STATE_KEY_TEXT_AREA
-  )
-  file_data = components.file_uploader(
-    constants.LANG_PACK.get("file_uploader_label"),
-    state.file_uploader_disabled,
-    constants.STATE_KEY_FILE_UPLOADER
-  )
-  btn = components.button(constants.LANG_PACK.get("btn_start_label"))
-  
-  # Получаем критерии (пропсы) для нашей модели, в нашем случае это просто текст
-  criteria = text_area_data or file_data
+    # Получаем состояние приложения
+    state = session.get_state()
+    # Рисуем основные компоненты приложения, используя состояние
+    text_area_data = components.text_area(
+        constants.LANG_PACK.get("text_area_label"),
+        state.text_area_disabled,
+        constants.STATE_KEY_TEXT_AREA,
+    )
+    file_data = components.file_uploader(
+        constants.LANG_PACK.get("file_uploader_label"),
+        state.file_uploader_disabled,
+        constants.STATE_KEY_FILE_UPLOADER,
+    )
+    btn = components.button(constants.LANG_PACK.get("btn_start_label"))
 
-  # Если все ок, то передаем в функцию процессинга, если нет, то пишем что не так
-  if bool(criteria) and btn:
-    with components.spinner(text=constants.LANG_PACK.get("loading_result_text")):
-      res = model.process(f'simplify | {criteria}', tokenizer, model_rut5)
+    # Получаем критерии (пропсы) для нашей модели, в нашем случае это просто текст
+    criteria = text_area_data or file_data
 
-    components.results(constants.LANG_PACK.get("result_text"), res)
-  elif not bool(criteria) and btn:
-    components.info(constants.LANG_PACK.get("empty_input_text"))
+    # Если все ок, то передаем в функцию процессинга, если нет, то пишем что не так
+    # if bool(criteria) and btn:
+    #     with components.spinner(text=constants.LANG_PACK.get("loading_result_text")):
+    #         res = model.process(f"simplify | {criteria}", tokenizer, model_rut5)
+
+    #     components.results(constants.LANG_PACK.get("result_text"), res)
+    # elif not bool(criteria) and btn:
+    #     components.info(constants.LANG_PACK.get("empty_input_text"))
+
+    # Если все ок, то передаем в функцию процессинга, если нет, то пишем что не так
+    if bool(criteria) and btn:
+        with components.spinner(text=constants.LANG_PACK.get("loading_result_text")):
+            res = model.process(f"simplify | {criteria}", tokenizer, model_rut5)
+
+        # Используем st.write() для вывода текста с переносами строк
+        st.write(constants.LANG_PACK.get("result_text"))
+
+        # Разделяем строки и выводим их отдельно
+        for line in res.split("\n"):
+            st.write(line)
+
+    elif not bool(criteria) and btn:
+        components.info(constants.LANG_PACK.get("empty_input_text"))
 
 
 if __name__ == "__main__":
